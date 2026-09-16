6784cf63cd670e752ad413efce7fe58d251214e9
rayen56-ops <rayen56@gmail.com>
2026-09-16 16:34:59 +0800

    Gameplay fix: treat grid origin as valid actor position

--- src/actions/attack_move_action.gd
+++ src/actions/attack_move_action.gd
@@ -1,43 +1,26 @@
 class_name AttackMoveAction
 extends ActorAction
 
-var direction: Vector2i
+
+func _init(delta: Vector2i) -> void:
+	self.delta = delta
 
 
-func _init(p_actor: Monster, dir: Vector2i) -> void:
-	super(p_actor)
-	direction = dir
-
-
-func _execute(map: Map, result: ActionResult) -> bool:
-	if not super(map, result):
+func perform() -> bool:
+	var map := World.current_map
+	if not map or not actor:
 		return false
 
 	var current_pos := map.find_monster_position(actor)
-	if not current_pos:
+	if current_pos == Utils.INVALID_POS:
 		return false
 
-	if actor.has_status_effect(StatusEffect.Type.CONFUSED):
-		direction = Utils.ALL_DIRECTIONS.pick_random()
+	var destination := current_pos + delta
+	if not map.is_in_bounds(destination):
+		return false
 
-	if actor.has_status_effect(StatusEffect.Type.PARALYZED):
-		if actor == World.player:
-			result.message = "You are paralyzed!"
-			result.message_level = LogMessages.Level.TERRIBLE
-		else:
-			result.message = "%s tries to move but is paralyzed!" % actor.name
+	var target := map.get_monster(destination)
+	if target and target != actor:
+		return MeleeAction.new(delta).set_actor(actor).perform()
 
-	var new_pos := current_pos + direction
-
-	# First try melee attack if there's a monster
-	if map.get_monster(new_pos):
-		var melee := MeleeAction.new(actor, direction)
-		return melee._execute(map, result)
-
-	# Otherwise try to move
-	var move := MoveAction.new(actor, direction)
-	return move._execute(map, result)
-
-
-func _to_string() -> String:
-	return "AttackMoveAction(actor: %s, direction: %s)" % [actor, direction]
+	return MoveAction.new(delta).set_actor(actor).perform()
